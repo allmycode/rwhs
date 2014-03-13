4e65013bef358239658ce3f297624daed36f9d2f
Stanislav Lyakhnovich <stas.lyakhnovich@gmail.com>
2014-03-13 14:35:18 +0400

    Exersise 3

--- Exersises.hs
+++ Exersises.hs
@@ -1,5 +1,10 @@
 -- ex. 1 Length function
 
 rwhsLength :: [a] -> Int
-rwhsLength (_:xs) = 1 + myLength xs
+rwhsLength (_:xs) = 1 + rwhsLength xs
 rwhsLength []   = 0
+
+-- ex 3. Mean function
+rwhsMean a = (snd (inner a))/(fromIntegral (fst (inner a)))
+         where inner [] = (0, 0)
+               inner (a:as) = (1 + fst (inner as), a + snd (inner as))
