f50f0e1b986b4aac4cb8f3055b4bac786ff9dfb3
Stanislav Lyakhnovich <stas.lyakhnovich@gmail.com>
2014-03-13 14:43:28 +0400

    Exersise 4 and 5

--- Exersises.hs
+++ Exersises.hs
@@ -8,3 +8,15 @@ rwhsLength []   = 0
 rwhsMean a = (snd (inner a))/(fromIntegral (fst (inner a)))
          where inner [] = (0, 0)
                inner (a:as) = (1 + fst (inner as), a + snd (inner as))
+
+-- ex 4/5. Turn list into palindrome.
+-- Write function to check if list is palindrome
+
+-- reverse is helper function for palindrome functions
+rwhsReverse (x:xs) = rev' (x:[]) xs
+  where rev' a (l:ls) = rev' (l:a) ls
+        rev' a [] = a
+
+rwhsMakePalindrome l = l ++ rwhsReverse l
+                   
+rwhsIsPalindrome l = l == rwhsReverse l
